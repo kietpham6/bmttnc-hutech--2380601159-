b588059c272cf0f20192550dfc66ef1dbed6a710
kietpham6 <kietpham612@gmail.com>
2026-05-15 09:23:47 +0700

    ex02_03 Kiemtrasochan

---
+++ lab-01/ex02_03.py
@@ -0,0 +1,7 @@
+#Nhập số người dùng
+so = int(input("Nhập một số nguyên: "))
+# Kiểm tra xem có phải chẵn hay không
+if so % 2 == 0:
+    print(so,"là số chẵn. ")
+else:
+    print(so,"không phải là số chẵn. ")
